74abee488eda44ac9de1743716e236ab17690b86
Hady Mohamed <hadymohamed@pursuit.org>
2021-06-08 10:55:50 -0400

    Initialized app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+// Dependencies
+const express = require("express");
+
+// Configuration
+const app = express();
+
+// Routes
+app.get("/", (req, res) => {
+  res.send("Welcome to the Express Minerals App!!");
+});
+
+// Export
+module.exports = app;
