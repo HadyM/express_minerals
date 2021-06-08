a826e2a605750caba6fd49db3f43f117f5c912d4
Hady Mohamed <hadymohamed@pursuit.org>
2021-06-08 11:37:55 -0400

    calling different rocks from /rock path

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // Dependencies
 const express = require("express");
+const rocks = require("./models/rock.js");
 
 // Configuration
 const app = express();
@@ -11,6 +12,15 @@ app.get("/", (req, res) => {
 
 app.get("/rocks", (req, res) => {
   res.send(rocks);
+});
+
+app.get("/rocks/:index", (req, res) => {
+  const { index } = req.params;
+  if (rocks[index]) {
+    res.send(rocks[index]);
+  } else {
+    res.send(`Sorry, no rock at index [${index}] exists!`);
+  }
 });
 
 // Export
