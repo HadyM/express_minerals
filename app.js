5e9d328dcbfc9b9cfa9587821cec090e3dd3ab16
Hady Mohamed <hadymohamed@pursuit.org>
2021-06-08 11:18:32 -0400

    added /rocks path

--- app.js
+++ app.js
@@ -9,5 +9,9 @@ app.get("/", (req, res) => {
   res.send("Welcome to the Express Minerals App!!");
 });
 
+app.get("/rocks", (req, res) => {
+  res.send(rocks);
+});
+
 // Export
 module.exports = app;
